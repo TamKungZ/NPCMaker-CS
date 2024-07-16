a35e0c0a808f9b6fd8937a3d1bf6821b1b6a18ae
TamKungZ_ <kittiwut.pimpromma@gmail.com>
2024-07-16 16:45:20 +0700

    Add files via upload

---
+++ NPCMaker/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace NPCMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // สร้างหรือโหลดหน้าต่างหลักของแอปพลิเคชัน
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            
+            // ทำการรันแอปพลิเคชัน
+            // เพิ่มโค้ดเพิ่มเติมตรงนี้ตามความต้องการ
+        }
+    }
+}
